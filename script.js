e43f9f8b6662d6a3e62bef894ad57f1624869c04
vatjsrg-pop <vatjsrg@gmail.com>
2025-10-30 10:56:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const cards = [
     { name: "daidaikichi", imageUrl: "img/daidaikiti_omikuji.jpeg" },
     { name: "tyukichi", imageUrl: "img/tyukiti_omikuji.jpeg" },
     { name: "kichi", imageUrl: "img/kiti_omikuji.jpeg" },
-    { name: "syokichi", imageUrl: "img/syoukiti_omikuji,jpeg" },
+    { name: "syokichi", imageUrl: "img/syoukiti_omikuji.jpeg" },
     { name: "suekiti", imageUrl: "img/suekiti_omikuji.jpeg" },
     { name: "daikiti", imageUrl: "img/daikiti_omikuji.jpeg" },
     { name: "kyou", imageUrl: "img/kyou_omikuji.jpeg" },
